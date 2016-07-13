2f66b062fd3f3db9923721fa0e602f12713242e2
nibb13 <nibble@list.ru>
2016-07-14 02:01:34 +0300

    IPv6 support (untested)

--- ip2loc-lean.sh
+++ ip2loc-lean.sh
@@ -43,12 +43,22 @@ parseOptions () {
 		$(updateDB);
 		if [ $? -eq 0 ]; then
 		    createIndex 1 1;
-		    createIndex 1 2;
+		    createIndex 2 1;
 		    flushTemp;
-		    setCronjob;
 		else
-    		    $PRINT "Autoupdate failed." >&2;
+    		    $PRINT "Autoupdate of IPv4 failed." >&2;
 		fi
+		
+		$(updateDB 1);
+		if [ $? -eq 0 ]; then
+		    createIndex 1 1 1;
+		    createIndex 2 1 1;
+		    flushTemp 1;    
+		else
+    		    $PRINT "Autoupdate of IPv6 failed." >&2;
+		fi
+		
+		setCronjob;
 		exit 0;
 		;;
 	    s)
@@ -96,7 +106,7 @@ usage () {
     $PRINT_E "\t-l\tLocation, lat & lon (i.e. 32.242850 -110.946248)";
     $PRINT_E "\t-z\tZIP/Postal code (i.e. 85719)";
     $PRINT_E "\t-t\tUTC time zone (with DST supported) (i.e. -07:00)";
-    $PRINT_E "\t-u\tUpdate database (internal use)";
+    $PRINT_E "\t-u\tUpdate databases (internal use)";
     $PRINT_E "\t-h\tUsage help";
     $PRINT;
 
@@ -119,6 +129,21 @@ getDBFile () {
 	    ;;
 	DB11LITE)
 	    $PRINT_N "IP2LOCATION-LITE-DB11";
+	    ;;
+	DB1LITEIPV6)
+	    $PRINT_N "IP2LOCATION-LITE-DB1.IPV6";
+	    ;;
+	DB3LITEIPV6)
+	    $PRINT_N "IP2LOCATION-LITE-DB3.IPV6";
+	    ;;
+	DB5LITEIPV6)
+	    $PRINT_N "IP2LOCATION-LITE-DB5.IPV6";
+	    ;;
+	DB9LITEIPV6)
+	    $PRINT_N "IP2LOCATION-LITE-DB9.IPV6";
+	    ;;
+	DB11LITEIPV6)
+	    $PRINT_N "IP2LOCATION-LITE-DB11.IPV6";
 	    ;;
     esac
 
@@ -208,12 +233,18 @@ downloadDB () {
     fi
     
     ZIP_FILE="$DATA_DIR/$DB_FILE_BASENAME.ZIP";
+    DB_CODE="$CONF_DB_CODE";
+    
+    if [ "$1" ]; then
+	ZIP_FILE="$DATA_DIR/$DB6_FILE_BASENAME.ZIP";
+	DB_CODE="$CONF_DB6_CODE";
+    fi
 
     if [ "$CONF_HTTP_BACKEND" = "curl" ]; then
-	curl -o "$ZIP_FILE" "http://www.ip2location.com/download?login=$CONF_IP2LOC_LOGIN&password=$CONF_IP2LOC_PASS&productcode=$CONF_DB_CODE" >/dev/null 2>&1
+	curl -o "$ZIP_FILE" "http://www.ip2location.com/download?login=$CONF_IP2LOC_LOGIN&password=$CONF_IP2LOC_PASS&productcode=$DB_CODE" >/dev/null 2>&1
 	ERROR_CODE=$?;
     elif [ "$CONF_HTTP_BACKEND" = "wget" ]; then
-	wget -O "$ZIP_FILE" -q "http://www.ip2location.com/download?login=$CONF_IP2LOC_LOGIN&password=$CONF_IP2LOC_PASS&productcode=$CONF_DB_CODE" >/dev/null 2>&1
+	wget -O "$ZIP_FILE" -q "http://www.ip2location.com/download?login=$CONF_IP2LOC_LOGIN&password=$CONF_IP2LOC_PASS&productcode=$DB_CODE" >/dev/null 2>&1
 	ERROR_CODE=$?;
     else
 	$PRINT "No http downloader found." >&2;
@@ -261,11 +292,17 @@ updateDB () {
 	exit 1;
     fi
 
-    ZIP_FILE=$(findDB "ZIP");
+    ZIP_FILE=$(findDB "ZIP" "$1");
+    
+    FILENAME="$DB_FILE_BASENAME.CSV";
+    
+    if [ "$1" ]; then
+	FILENAME="$DB6_FILE_BASENAME.CSV";
+    fi
     
     if [ ! "$ZIP_FILE" ]; then
 	$PRINT "DB not found. Downloading..." >&2;
-	RESULT=$(downloadDB);
+	RESULT=$(downloadDB "$1");
 	if [ "$?" -ne 0 ]; then
 	    $PRINT "Error while downloading:" >&2;
 	    $PRINT "$RESULT" >&2;
@@ -278,18 +315,18 @@ updateDB () {
     if [ -f "$ZIP_FILE" ]; then
     
 	$PRINT "Got zip file at $ZIP_FILE, unpacking..." >&2;
-	unzip "$ZIP_FILE" "$DB_FILE_BASENAME.CSV" -d "$DATA_DIR/tmp" >/dev/null 2>&1;
+	unzip "$ZIP_FILE" "$FILENAME" -d "$DATA_DIR/tmp" >/dev/null 2>&1;
 	EXIT_CODE=0;
 	if [ "$?" -ne 0 ]; then
 	    $PRINT "Error unpacking $ZIP_FILE." >&2;
 	    EXIT_CODE=1;
 	fi
-	chmod 644 "$DATA_DIR/tmp/$DB_FILE_BASENAME.CSV";
-	rm "$ZIP_FILE";
-	if [ "$?" -ne 0 ]; then
-	    $PRINT "Error removing $ZIP_FILE." >&2
-	    EXIT_CODE=1;
-	fi
+	chmod 644 "$DATA_DIR/tmp/$FILENAME";
+	#rm "$ZIP_FILE";
+	#if [ "$?" -ne 0 ]; then
+	#    $PRINT "Error removing $ZIP_FILE." >&2
+	#    EXIT_CODE=1;
+	#fi
 	
 	$PRINT_EN "$ZIP_FILE";
 	exit $EXIT_CODE;
@@ -303,15 +340,21 @@ createIndex () {
     if [ "$2" ]; then
 	DIR="tmp/";
     fi
+    
+    FILENAME="$DB_FILE_BASENAME";
+    
+    if [ "$3" ]; then
+	FILENAME="$DB6_FILE_BASENAME";
+    fi
 
     if [ "$1" -eq 1 ]; then
 	DELIMITER="\"*,\"*";
-	IN_FILE="$DATA_DIR/$DIR$DB_FILE_BASENAME.CSV";
-	OUT_FILE="$DATA_DIR/$DIR$DB_FILE_BASENAME.IDX";
+	IN_FILE="$DATA_DIR/$DIR$FILENAME.CSV";
+	OUT_FILE="$DATA_DIR/$DIR$FILENAME.IDX";
     elif [ "$1" -eq 2 ]; then
 	DELIMITER=" ";
-	IN_FILE="$DATA_DIR/$DIR$DB_FILE_BASENAME.IDX";
-	OUT_FILE="$DATA_DIR/$DIR$DB_FILE_BASENAME.ID2";
+	IN_FILE="$DATA_DIR/$DIR$FILENAME.IDX";
+	OUT_FILE="$DATA_DIR/$DIR$FILENAME.ID2";
     fi
     
     $CONF_AWK_BACKEND -F "$DELIMITER"  "BEGIN{NR=-1; totalBytes=0; prevBytes=0; } { prevBytes = totalBytes; totalBytes += length(\$0) + 1; gsub(/\"/, \"\"); if (NR % 100 == 0) { print \$1 \" \" prevBytes; }} END { print \$1 \" \" totalBytes; }" "$IN_FILE"  > "$OUT_FILE";
@@ -321,10 +364,16 @@ createIndex () {
 flushTemp () {
 
     #TODO: make wait for other ip2loc-lean processes exit.
+    
+    FILENAME="$DB_FILE_BASENAME";
 
-    mv -f "$DATA_DIR/tmp/$DB_FILE_BASENAME.CSV" "$DATA_DIR/";
-    mv -f "$DATA_DIR/tmp/$DB_FILE_BASENAME.IDX" "$DATA_DIR/";
-    mv -f "$DATA_DIR/tmp/$DB_FILE_BASENAME.ID2" "$DATA_DIR/";
+    if [ "$1" ]; then
+	FILENAME="$DB6_FILE_BASENAME";
+    fi
+
+    mv -f "$DATA_DIR/tmp/$FILENAME.CSV" "$DATA_DIR/";
+    mv -f "$DATA_DIR/tmp/$FILENAME.IDX" "$DATA_DIR/";
+    mv -f "$DATA_DIR/tmp/$FILENAME.ID2" "$DATA_DIR/";
     if [ "$DATA_DIR" ]; then
 	rm -rf "$DATA_DIR/tmp";
     fi;
@@ -393,52 +442,52 @@ fi
 
 # TODO: Fix this ^
 
-DB_FILE=$(findDB);
-DB6_FILE=$(findDB "" "1");
+DB_FILE=$(findDB "" "$IS_IPV6");
 
 if [ ! "$DB_FILE" ]; then
     
     $PRINT "Downloading db" >&2;
     
-    ZIP_FILE=$(updateDB);
+    ZIP_FILE=$(updateDB "$IS_IPV6");
 
     if [ $? -eq 0 ]; then
 	$PRINT "Downloaded, creating index" >&2;
-	createIndex 1 1;
-	createIndex 2 1;
-	flushTemp;
+	createIndex 1 1 "$IS_IPV6";
+	createIndex 2 1 "$IS_IPV6";
+	flushTemp "$IS_IPV6";
 	setCronjob;
     else
+	test "$IS_IPV4" && $PRINT_N "IPv4 " || $PRINT_N "IPv6 ";
         $PRINT "DB not found." >&2;
     fi
     
 fi
 
-DB_FILE=$(findDB);
+DB_FILE=$(findDB "" "$IS_IPV6");
 
 if [ "$DB_FILE" ]; then
     
-    INDEX_FILE=$(findDB "IDX");
+    INDEX_FILE=$(findDB "IDX" "$IS_IPV6");
     
     if [ ! "$INDEX_FILE" ]; then
 	$PRINT_N "Creating primary index..." >&2;
-        createIndex 1;
+        createIndex 1 "" "$IS_IPV6";
         $PRINT " OK!" >&2;
     fi
     
-    INDEX_FILE=$(findDB "IDX");
+    INDEX_FILE=$(findDB "IDX" "$IS_IPV6");
     
     if [ -f "$INDEX_FILE" ]; then
     
-	INDEX2_FILE=$(findDB "ID2");
+	INDEX2_FILE=$(findDB "ID2" "$IS_IPV6");
     
 	if [ ! "$INDEX2_FILE" ]; then
 	    $PRINT_N "Creating secondary index..." >&2;
-	    createIndex 2;
+	    createIndex 2 "" "$IS_IPV6";
 	    $PRINT " OK!" >&2;
         fi
         
-        INDEX2_FILE=$(findDB "ID2");
+        INDEX2_FILE=$(findDB "ID2" "$IS_IPV6");
         
         if [ ! "$INDEX2_FILE" ]; then
     	    $PRINT "Cannot find nor create secondary index file." >&2;
@@ -452,25 +501,148 @@ fi
 
 if [ "$INDEX_FILE" ] && [ "$INDEX2_FILE" ]; then
 
-    IP_NUMBER=$($PRINT "$IP_ADDRESS" | $CONF_AWK_BACKEND -F "." '{print 16777216 * $1 + 65536 * $2 + 256 * $3 + $4;}');
+    if [ "$IS_IPV4" ]; then
+	IP_NUMBER=$($PRINT "$IP_ADDRESS" | $CONF_AWK_BACKEND -F "." '{print 16777216 * $1 + 65536 * $2 + 256 * $3 + $4;}');
+    else
+	$PRINT "Expanding IPv6 address...";
+	IP_ADDRESS=$($PRINT_EN "$IP_ADDRESS" | awk '
+{
+
+ORS="";
+
+count = split($0, condensedParts, "::");
+
+if (count == 1){
+    fieldsCount = split($0, fields, ":");
+} else {
+    part1Count = split(condensedParts[1], part1, ":");
+    part2Count = split(condensedParts[2], part2, ":");
+    
+    missingFieldsCount = 8 - (part1Count + part2Count);
+    zeroes = "0000";
+    for (i=1; i<missingFieldsCount; i++){
+	zeroes = zeroes"0000";
+    }
+    
+    for (i=1; i<=part1Count; i++){
+	fields[i] = part1[i];
+    }
+    
+    fields[part1Count+1] = zeroes;
+    
+    for (i=1; i<=part2Count; i++){
+	fields[i+part1Count+1] = part2[i];
+    }
+    
+    fieldsCount = part1Count + part2Count + 1;
+    
+}
+
+for (i=1; i<=fieldsCount; i++){
+    pad = 4 - length(fields[i]);
+    for (j=1; j<=pad; j++){print "0"};
+    print toupper(fields[i]);
+}
+
+}');
+
+    $PRINT "Done, result: $IP_ADDRESS";
+
+	if [ "$CONF_BC_PRESENT" ]; then
+		$PRINT "bc is present, using it";
+		IP_NUMBER=$($PRINT "ibase=16;$IP_ADDRESS" | bc);
+		$PRINT "Done, number: $IP_NUMBER";
+	else
+		IP_NUMBER=$($PRINT_N "$IP_ADDRESS" | awk '
+
+function arr_add(one, two, oneLength, twoLength)
+{
+
+	c = 0;
+	rCount = 0;
+	
+	if (one[1] == "-") {onePad = 1;} else {onePad = 0;}
+	if (two[1] == "-") {twoPad = 1;} else {twoPad = 0;}
+
+	oneIndex = oneLength + onePad;
+	twoIndex = twoLength + twoPad;
+
+	do {
+
+		elemOne = one[oneIndex];
+		elemTwo = two[twoIndex];
+		if (elemOne == "" || elemOne == "-") {elemOne=0}
+		if (elemTwo == "" || elemTwo == "-") {elemTwo=0}
+
+		s = elemOne + elemTwo + c;
+
+		if (s < 10){
+			one[oneIndex + (1 - onePad)] = s;
+			c = 0;
+		} else {
+			one[oneIndex + (1 - onePad)] = s - 10;
+			c = 1;
+		}
+		
+		oneIndex--;
+		twoIndex--;
+		rCount++;
+
+	} while(twoIndex - twoPad > 0 || onePad == 0 && oneIndex > 0)
+	
+	if (c){
+	    one[1]=1;
+	    rCount++;
+	} else if (!onePad){one[1] = "-";}
+	
+	if (rCount > oneLength){return rCount};
+	return oneLength;
+
+}
+
+{
+
+ORS="";
+
+dec[1] = 0;
+digit[1] = 1;
+rLength = 1;
+digitsCount = split($0, digits, "");
+for (j=1; j<=digitsCount; j++){
+	n = int("0x"digits[j]);
+	for (t=8; t>0; t=rshift(t,1)){
+		rLength = arr_add(dec, dec, rLength, rLength);
+		if (and(n, t) != 0) {rLength = arr_add(dec, digit, rLength, 1)}
+	}
+}
+
+if (dec[1] == "-") {pad = 1;} else {pad = 0;}
+
+for (i=1; i<=rLength+pad; i++){
+	print dec[i+pad];
+}
+
+}');
+	fi
+    fi
     
     SKIP_BYTES=$(awk "{if (\$1 > $IP_NUMBER) {exit 0;} byteskip=\$2} END {print byteskip;}" "$INDEX2_FILE");
     SKIP_BYTES=$(dd if="$INDEX_FILE" bs=1 skip=$SKIP_BYTES 2>/dev/null | awk "{if (\$1 > $IP_NUMBER) {exit 0;} byteskip=\$2} END {print byteskip;}");
     
     case $CONF_DB_CODE in
-	DB1LITE)
+	DB1LITE*)
 	    I=4;
 	    ;;
-	DB3LITE)
+	DB3LITE*)
 	    I=6;
 	    ;;
-	DB5LITE)
+	DB5LITE*)
 	    I=8;
 	    ;;
-	DB9LITE)
+	DB9LITE*)
 	    I=9;
 	    ;;
-	DB11LITE)
+	DB11LITE*)
 	    I=10;
 	    ;;
     esac
